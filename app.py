facb1d470dd9a84430549aab9a654cb75f543221
Jeremy Nelson <jermnelson.gmail.com>
2015-08-14 12:49:12 -0600

    Added Skeleton CSS and stub pages for presentation

---
+++ app.py
@@ -0,0 +1,13 @@
+__author__ = "Jeremy Nelson"
+__license__ = "GPLv3"
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=20159, debug=True) 
